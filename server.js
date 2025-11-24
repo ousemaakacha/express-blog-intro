10340b4de1e729750fe9e62f08d27b7c6eb317ab
ousemaakacha <akachaousema@gmail.com>
2025-11-24 12:16:49 +0100

    feat: object array addition

--- server.js
+++ server.js
@@ -1,7 +1,37 @@
 const express = require("express");
+const { title } = require("process");
 const app = express();
 const PORT = 3000;
 
 app.use(express.static("public"));
 
-const posts = [{}];
+const posts = [
+  {
+    title: "CIAMBELLOONEEE",
+    image: "./images/ciambellone.jpeg",
+    tags: ["dolci"],
+  },
+
+  {
+    title: "CRACKER BARBABIETOLA",
+    image: "./images/cracker-barbabietola.jpeg",
+    tags: ["salati"],
+  },
+  {
+    title: "PANE FRITTO DOLCE",
+    image: "./images/pane-fritto-dolce.jpeg",
+    tags: ["dolci"],
+  },
+
+  {
+    title: "PASTA BARBABIETOLA",
+    image: "./images/pasta-barbabietola.jpeg",
+    tags: ["salati"],
+  },
+
+  {
+    title: "TORTA PAESANA",
+    image: "./images/torta-paesana.jpeg",
+    tags: ["dolci"],
+  },
+];
