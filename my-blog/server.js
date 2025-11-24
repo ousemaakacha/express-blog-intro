6049679d144b6f5e7d4cab4111368879ad9589ff
ousemaakacha <akachaousema@gmail.com>
2025-11-24 12:32:03 +0100

    feat: get res for understanding if server works using listen call

--- my-blog/server.js
+++ my-blog/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { title } = require("process");
 const app = express();
 const PORT = 3000;
 
@@ -8,30 +7,40 @@ app.use(express.static("public"));
 const posts = [
   {
     title: "CIAMBELLOONEEE",
-    image: "./images/ciambellone.jpeg",
+    image: "/images/ciambellone.jpeg",
     tags: ["dolci"],
   },
 
   {
     title: "CRACKER BARBABIETOLA",
-    image: "./images/cracker-barbabietola.jpeg",
+    image: "/images/cracker-barbabietola.jpeg",
     tags: ["salati"],
   },
   {
     title: "PANE FRITTO DOLCE",
-    image: "./images/pane-fritto-dolce.jpeg",
+    image: "/images/pane-fritto-dolce.jpeg",
     tags: ["dolci"],
   },
 
   {
     title: "PASTA BARBABIETOLA",
-    image: "./images/pasta-barbabietola.jpeg",
+    image: "/images/pasta-barbabietola.jpeg",
     tags: ["salati"],
   },
 
   {
     title: "TORTA PAESANA",
-    image: "./images/torta-paesana.jpeg",
+    image: "/images/torta-paesana.jpeg",
     tags: ["dolci"],
   },
 ];
+
+// inizio a scrivere la rotta pricipalke
+
+app.get("/", (req, res) => {
+  res.send("My blog Server");
+});
+
+app.listen(PORT, () => {
+  console.log(`Blog server starts at http://localhost:${PORT}`);
+});
